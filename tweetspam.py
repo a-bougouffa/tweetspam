c38651297677fc1e7998d71ec7c3e740cbbbf0b1
Abdelhak Bougouffa <abdelhak@cryptolab.net>
2014-05-17 12:29:54 +0100

    version 0.1

--- tweetspam.py
+++ tweetspam.py
@@ -1,4 +1,16 @@
+# NOTE: I write this just for fun, don't use it! don't be evil ^_^
+
 import subprocess
-tweet = input("Tweet: ")
-twitter_tool = 'twitter -eabdelhak.alg@gmail.com set %s' % tweet
-subprocess.call(twitter_tool, shell=True)
+import random
+
+list_des_mots = (	"Je suis au #algeria20",
+					"rahom ya7ko 3la les #SOCIAL_MEDIA #algeria20"
+					"O.o #algeria20",
+					":P #algeria20",
+					"-_- 'social!!!' MEDIA!! #algeria20",
+					"je suis un SPAM #algeria20")
+
+while True:
+	twitter_tool = 'twitter -eabdelhak.alg@gmail.com set %s' % random.choice(list_des_mots)
+	subprocess.call(twitter_tool, shell=True)
+	print("Twitted ", end=" ")
